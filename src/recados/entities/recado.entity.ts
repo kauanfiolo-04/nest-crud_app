ffc4146f5aa3c22631404b6a9b3a39867762126f
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-01-05 17:09:32 -0300

    Criando Table ausando @Entity do TypeOrm

--- src/recados/entities/recado.entity.ts
+++ src/recados/entities/recado.entity.ts
@@ -1,8 +1,28 @@
-export interface Recado {
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Recado {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   texto: string;
+
+  @Column({ type: 'varchar', length: 255 })
   de: string;
+
+  @Column({ type: 'varchar', length: 255 })
   para: string;
+
+  @Column({ default: false })
   lido: boolean;
+
+  @Column()
   data: Date;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatededAt?: Date;
 }
