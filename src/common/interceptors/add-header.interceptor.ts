fbfdb103677607cd86570725dbd333c992519d1d
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-01-09 13:53:21 -0300

    Criando interceptor

---
+++ src/common/interceptors/add-header.interceptor.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+export class AddHeaderInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+    const response = context.switchToHttp().getResponse();
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    response.setHeader('X-Custom-Header', 'O valor do cabeçalho');
+
+    return next.handle();
+  }
+}
