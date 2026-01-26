9558d0e09af954a9e3aa57683d7fe42e4e601f07
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-01-26 15:55:21 -0300

    Usando o joi para validaçao de schema do .env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,23 @@ import { RecadosModule } from '../recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from '../pessoas/pessoas.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       // envFilePath: ['.env'],
-      // ignoreEnvFile: true
+      // ignoreEnvFile: true,
+      validationSchema: Joi.object({
+        DATABASE_TYPE: Joi.required(),
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+        DATABASE_USERNAME: Joi.required(),
+        DATABASE_DATABASE: Joi.required(),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_AUTOLOADENTITIES: Joi.number().min(0).max(1).default(0),
+        DATABASE_SYNCHRONIZE: Joi.number().min(0).max(1).default(0)
+      })
     }),
     TypeOrmModule.forRoot({
       type: process.env.DATABASE_TYPE as 'postgres',
