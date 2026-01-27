f5dcddcdcffaafaa1ea11a3ce1e39cc1d77a1243
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-01-27 15:27:13 -0300

    erro de ortografia estava quebrando minhas entidades

--- src/global-config/global.config.ts
+++ src/global-config/global.config.ts
@@ -8,7 +8,7 @@ export default registerAs('globalConfig', () => ({
     username: process.env.DATABASE_USERNAME,
     database: process.env.DATABASE_DATABASE,
     password: process.env.DATABASE_PASSWORD,
-    autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADINGENTITIES),
+    autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES || true),
     synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE)
   },
   environment: process.env.NODE_ENV || 'development'
