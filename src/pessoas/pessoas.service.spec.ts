35ee2aac95a854b1c43517ff41ec3c1d27cf195e
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-02-11 11:04:30 -0300

    Instanciando modulo PessoaService pra poder testar e definindo o q vai ser testado

--- src/pessoas/pessoas.service.spec.ts
+++ src/pessoas/pessoas.service.spec.ts
@@ -1,14 +1,47 @@
+import { Repository } from 'typeorm';
+import { PessoasService } from './pessoas.service';
+import { Pessoa } from './entities/pessoa.entity';
+import { HashingService } from '../auth/hashing/hashing.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
 describe('Pessoas service', () => {
-  beforeEach(() => {
-    console.log('Isso roda antes de cada teste');
+  let pessoaService: PessoasService;
+  let pessoaRepository: Repository<Pessoa>;
+  let hashingService: HashingService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PessoasService,
+        {
+          provide: getRepositoryToken(Pessoa),
+          useValue: {}
+        },
+        {
+          provide: HashingService,
+          useValue: {}
+        }
+      ]
+    }).compile();
+
+    pessoaService = module.get<PessoasService>(PessoasService);
+    pessoaRepository = module.get<Repository<Pessoa>>(getRepositoryToken(Pessoa));
+    hashingService = module.get<HashingService>(HashingService);
   });
 
-  it('It Alguma coisa', () => {
-    const numero1 = 1;
-    const numero2 = 2;
+  it('pessoaService should be defined', () => {
+    expect(pessoaService).toBeDefined();
+  });
 
-    const result = numero1 + numero2;
-
-    expect(result).toBe(3);
+  describe('create', () => {
+    it('should create a new person', () => {
+      // CreatePessoaDTO
+      // Que o hashing service tenha o método hash
+      // Saber se o hashing service foi chamado com CreatePessoaDTO
+      // Saber se o pessoaRepository.create foi chamado com dados pessoa
+      // Saber se pessoaRepository.save foi chamado com a pessoa criada
+      // O retorno final deve ser a nova pessoa criada -> expect
+    });
   });
 });
