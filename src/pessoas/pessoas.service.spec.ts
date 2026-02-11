a51e14ca2d0e5113c95363f94f47f1fe16cb1be5
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-02-11 12:19:43 -0300

    Add unit test for create method in PessoasService with CreatePessoaDto

--- src/pessoas/pessoas.service.spec.ts
+++ src/pessoas/pessoas.service.spec.ts
@@ -4,6 +4,7 @@ import { Pessoa } from './entities/pessoa.entity';
 import { HashingService } from '../auth/hashing/hashing.service';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { CreatePessoaDto } from './dto/create-pessoa.dto';
 
 describe('Pessoas service', () => {
   let pessoaService: PessoasService;
@@ -16,11 +17,16 @@ describe('Pessoas service', () => {
         PessoasService,
         {
           provide: getRepositoryToken(Pessoa),
-          useValue: {}
+          useValue: {
+            create: jest.fn(),
+            save: jest.fn()
+          }
         },
         {
           provide: HashingService,
-          useValue: {}
+          useValue: {
+            hash: jest.fn()
+          }
         }
       ]
     }).compile();
@@ -35,13 +41,37 @@ describe('Pessoas service', () => {
   });
 
   describe('create', () => {
-    it('should create a new person', () => {
+    it('should create a new person', async () => {
       // CreatePessoaDTO
       // Que o hashing service tenha o método hash
       // Saber se o hashing service foi chamado com CreatePessoaDTO
       // Saber se o pessoaRepository.create foi chamado com dados pessoa
       // Saber se pessoaRepository.save foi chamado com a pessoa criada
       // O retorno final deve ser a nova pessoa criada -> expect
+
+      // Arrange
+      const createPessoaDto: CreatePessoaDto = {
+        email: 'JEST@email.com',
+        nome: 'Jest da Silva',
+        password: '123456'
+      };
+
+      const hashSpy = jest.spyOn(hashingService, 'hash').mockResolvedValue('HASH_DE_SENHA');
+      // const createSpy = jest.spyOn(pessoaRepository, 'create');
+
+      // Act
+      await pessoaService.create(createPessoaDto);
+
+      // Assert
+      expect(hashSpy).toHaveBeenCalledWith(createPessoaDto.password);
+
+      // Posso usar o createSpy pra n dar erro no eslint
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(pessoaRepository.create).toHaveBeenCalledWith({
+        nome: createPessoaDto.nome,
+        email: createPessoaDto.email,
+        passwordHash: 'HASH_DE_SENHA'
+      });
     });
   });
 });
