9982e25862182def4adce18fa8d30997ec3f5960
kauanfiolo-04 <kauanfiolo@hotmail.com>
2026-02-11 10:10:38 -0300

    escrevendo primeiro teste

---
+++ src/pessoas/pessoas.service.spec.ts
@@ -0,0 +1,14 @@
+describe('Pessoas service', () => {
+  beforeEach(() => {
+    console.log('Isso roda antes de cada teste');
+  });
+
+  it('It Alguma coisa', () => {
+    const numero1 = 1;
+    const numero2 = 2;
+
+    const result = numero1 + numero2;
+
+    expect(result).toBe(3);
+  });
+});
